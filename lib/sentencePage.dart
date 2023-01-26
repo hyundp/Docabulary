d3586a11c54e63a36fdcc2a5f1e78113333c2a5f
dyun <zkdlxkxks@nate.com>
2023-01-26 21:58:12 +0900

    add: sentence page

---
+++ lib/sentencePage.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class SentencePage extends StatefulWidget {
+  @override
+  _SentencePage createState() => _SentencePage();
+}
+
+class _SentencePage extends State<SentencePage> {
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('문장'),
+        ),
+        body: Container(
+            child: const Center(
+              child: Text('Sentence Page'),
+            )
+        )
+    );
+  }
+}
+
